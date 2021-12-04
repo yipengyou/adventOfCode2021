231ffef3e51602166d4def9fdfa96bc5f40672e5
Anson Chang <990722ansonchang@gmail.com>
2021-12-04 17:50:02 -0500

    doesnt work yet day 4

--- day4.py
+++ day4.py
@@ -3,12 +3,40 @@ inputs = file.readline()
 cnt = 0
 bingo = []
 ans = []
+d = {}
+appended = False
 for a in file:
     if a != '\n':
-        bingo.append([a])
+        
+        temp = a[:-1].split(' ')
+        
+        for i in range(temp.count('')):
+            temp.remove('')
+        
+        bingo.append(temp)
         cnt += 1
 
         if cnt == 5:
-            print(bingo)
+            for i in range(len(inputs)):
+                for x in range(len(bingo)):
+                    for y in range(len(bingo[x])):
+                        if bingo[x][y] == inputs[i]:
+                            bingo[x].pop(y)
+                for w in bingo:
+                    if w == []:
+                        ans.append((i, inputs[i] * sum([q for e in bingo for q in e])))
+                        appended = True
+                        break
+                if appended:
+                    break
+                else:
+                    if len(bingo[0]) == 4 and len(bingo[1]) == 4 and len(bingo[2]) == 4 and len(bingo[3]) == 4 and len(bingo[4]) == 4:
+                        ans.append(i, inputs[i] * sum([q for e in bingo for q in e]))
+                        appended = True
+                if appended:
+                    break
             bingo = []
-            break
+            cnt = 0
+            appended = False
+ans.sort()
+print(ans[0][1])
