6e6053a9970691ff84094436674c0090f75747b2
Anson Chang <990722ansonchang@gmail.com>
2021-12-09 15:15:55 -0500

    very inefficient solution

--- day9.py
+++ day9.py
@@ -11,29 +11,57 @@ seen = []
 ###part 1
 
 
-# def dfs(x,y, seen):
-#     if [x,y] not in seen:
-#         seen.append([x,y])
-#         if x + 1 < len(matrix[0]) and matrix[y][x+1] <= matrix[y][x]:
-#             dfs(x + 1, y, seen)
-#         elif x - 1 >= 0 and matrix[y][x-1] <= matrix[y][x]:
-#             dfs(x-1, y, seen)
-#         elif y + 1 < len(matrix) and matrix[y+1][x] <= matrix[y][x]:
-#             dfs(x, y+1, seen)
-#         elif y - 1 >= 0 and matrix[y-1][x] <= matrix[y][x]:
-#             dfs(x, y-1, seen)
-#         else:
-#             ans.append(matrix[y][x])
+def dfs(x,y):
+    if [x,y] not in seen:
+        seen.append([x,y])
+        if x + 1 < len(matrix[0]) and matrix[y][x+1] <= matrix[y][x]:
+            dfs(x + 1, y)
+        elif x - 1 >= 0 and matrix[y][x-1] <= matrix[y][x]:
+            dfs(x-1, y)
+        elif y + 1 < len(matrix) and matrix[y+1][x] <= matrix[y][x]:
+            dfs(x, y+1)
+        elif y - 1 >= 0 and matrix[y-1][x] <= matrix[y][x]:
+            dfs(x, y-1)
+        else:
+            ans.append([x,y])
 
-# for a in range(len(matrix)):
-#     for b in range(len(matrix[0])):
-#         if matrix[a][b] not in seen:
-#             dfs(b, a, seen)
-# print(ans)
-# print(sum([1+i for i in ans]))
+for a in range(len(matrix)):
+    for b in range(len(matrix[0])):
+        if matrix[a][b] not in seen:
+            dfs(b, a)
+            
 
 ###part 2
+basin = []
+anss = []
 
-def dfs(x,y, seen):
-    if [x,y] not in seen:
-        if x + 1 < len(matrix[0]) and matrix[y]
+def dfs2(x,y, seen):
+    
+    if [x,y] not in seen and matrix[y][x] != 9:
+        
+        if [x,y] not in basin: basin.append([x,y])
+            
+        if x + 1 < len(matrix[0]) and matrix[y][x+1] >= matrix[y][x] :
+            
+            dfs2(x + 1, y, seen + [[x, y]])
+            
+        if x - 1 >= 0 and matrix[y][x-1] >= matrix[y][x]:
+            
+            dfs2(x-1, y, seen + [[x, y]])
+            
+        if y + 1 < len(matrix) and matrix[y+1][x] >= matrix[y][x]:
+            
+            dfs2(x, y + 1, seen + [[x, y]])
+            
+        if y -1 >= 0 and matrix[y-1][x] >= matrix[y][x]:
+            
+            dfs2(x, y-1, seen + [[x, y]])
+
+for a in ans:
+    dfs2(a[0], a[1], [])
+    anss.append(basin)
+    basin = []
+
+anss = sorted(anss, key=len)
+print(len(anss[-1]) * len(anss[-2]) * len(anss[-3]))
+        
