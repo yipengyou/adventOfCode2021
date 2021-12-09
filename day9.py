65a6249da581925fd07a729e5a7020af3dbda232
Anson <990722ansonchang@gmail.com>
2021-12-09 12:15:08 -0500

    mfmdsfsa

--- day9.py
+++ day9.py
@@ -8,23 +8,32 @@ for a in file:
 ans = []
 seen = []
 
+###part 1
+
+
+# def dfs(x,y, seen):
+#     if [x,y] not in seen:
+#         seen.append([x,y])
+#         if x + 1 < len(matrix[0]) and matrix[y][x+1] <= matrix[y][x]:
+#             dfs(x + 1, y, seen)
+#         elif x - 1 >= 0 and matrix[y][x-1] <= matrix[y][x]:
+#             dfs(x-1, y, seen)
+#         elif y + 1 < len(matrix) and matrix[y+1][x] <= matrix[y][x]:
+#             dfs(x, y+1, seen)
+#         elif y - 1 >= 0 and matrix[y-1][x] <= matrix[y][x]:
+#             dfs(x, y-1, seen)
+#         else:
+#             ans.append(matrix[y][x])
+
+# for a in range(len(matrix)):
+#     for b in range(len(matrix[0])):
+#         if matrix[a][b] not in seen:
+#             dfs(b, a, seen)
+# print(ans)
+# print(sum([1+i for i in ans]))
+
+###part 2
+
 def dfs(x,y, seen):
     if [x,y] not in seen:
-        seen.append([x,y])
-        if x + 1 < len(matrix[0]) and matrix[y][x+1] <= matrix[y][x]:
-            dfs(x + 1, y, seen)
-        elif x - 1 >= 0 and matrix[y][x-1] <= matrix[y][x]:
-            dfs(x-1, y, seen)
-        elif y + 1 < len(matrix) and matrix[y+1][x] <= matrix[y][x]:
-            dfs(x, y+1, seen)
-        elif y - 1 >= 0 and matrix[y-1][x] <= matrix[y][x]:
-            dfs(x, y-1, seen)
-        else:
-            ans.append(matrix[y][x])
-
-for a in range(len(matrix)):
-    for b in range(len(matrix[0])):
-        if matrix[a][b] not in seen:
-            dfs(b, a, seen)
-print(ans)
-print(sum([1+i for i in ans]))
+        if x + 1 < len(matrix[0]) and matrix[y]
