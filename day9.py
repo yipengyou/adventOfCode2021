d11b57d7616ad1bc587c20178e68335eae62b5df
Anson <990722ansonchang@gmail.com>
2021-12-09 02:51:28 -0500

    jjkj

--- day9.py
+++ day9.py
@@ -6,24 +6,25 @@ for a in file:
     matrix.append([int(i)  for i in a if i != '\n'])
 
 ans = []
+seen = []
 
 def dfs(x,y, seen):
     if [x,y] not in seen:
+        seen.append([x,y])
         if x + 1 < len(matrix[0]) and matrix[y][x+1] <= matrix[y][x]:
-            dfs(x + 1, y, seen + [[x,y]])
+            dfs(x + 1, y, seen)
         elif x - 1 >= 0 and matrix[y][x-1] <= matrix[y][x]:
-            dfs(x-1, y, seen + [[x,y]])
+            dfs(x-1, y, seen)
         elif y + 1 < len(matrix) and matrix[y+1][x] <= matrix[y][x]:
-            dfs(x, y+1, seen + [[x,y]])
+            dfs(x, y+1, seen)
         elif y - 1 >= 0 and matrix[y-1][x] <= matrix[y][x]:
-            dfs(x, y-1, seen + [[x,y]])
+            dfs(x, y-1, seen)
         else:
             ans.append(matrix[y][x])
-        matrix[x][y] = 'a'
 
 for a in range(len(matrix)):
     for b in range(len(matrix[0])):
-        if matrix[a][b] != 'a':
-            dfs(b, a, [])
-
+        if matrix[a][b] not in seen:
+            dfs(b, a, seen)
+print(ans)
 print(sum([1+i for i in ans]))
