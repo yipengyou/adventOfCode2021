ec9482aa56ee138e7644880dc390cdc02eb0330d
Anson Chang <990722ansonchang@gmail.com>
2021-12-10 16:55:04 -0500

    wasted a lot of time but finally done

--- day10.py
+++ day10.py
@@ -6,11 +6,13 @@ dicks = { ')': 3,
 inputs = open("day10_inputs.txt")
 
 stack = []
+unfinished = []
 
 sums = 0
 
 ###part1
 for a in inputs:
+    stack = []
     stack.append(a[0])
     if a[-1] == '\n':
         a = a[:-1]
@@ -19,19 +21,42 @@ for a in inputs:
             if i == ']':
                 if stack.pop(-1) != '[':
                     sums += dicks[i]
+                    stack = []
                     break
             elif i == '}':
                 if stack.pop(-1) != '{':
                     sums += dicks[i]
+                    stack = []
                     break
             elif i == '>':
                 if stack.pop(-1) != '<':
                     sums += dicks[i]
+                    stack = []
                     break
             elif i == ')':
                 if stack.pop(-1) != '(':
                     sums += dicks[i]
+                    stack = []
                     break
             else:
                 stack.append(i)
-print(sums)
+        if stack:
+            unfinished.append(stack)
+
+sums = []
+
+dick = {'(':1,
+        '[':2,
+        '{':3,
+        '<':4
+        }
+
+
+for a in unfinished:
+    temp = 0
+    for i in a[::-1]:
+        temp *= 5
+        temp += dick[i]
+    sums.append(temp)
+sums.sort()
+print(sums[len(sums)// 2])
