92c01d4823d3a973e16c69f475bd901dce1c5807
Anson Chang <990722ansonchang@gmail.com>
2021-12-15 17:04:39 -0500

    changed dijk to dik

--- day15.py
+++ day15.py
@@ -11,28 +11,32 @@ for y in range(len(cave)):
 risk[(0,0)] = 0
 
 visted = []
-def dijk(x, y, cost):
-    print(x,y)
+
+def dik(x, y, cost):
     visted.append((x,y))
     temp = []
     if (x + 1, y) not in visted and (x+1, y) in risk:
         
         risk[(x+1,y)] = min(cost + cave[y][x+1], risk[(x+1, y)])
         temp.append((risk[(x+1, y)], (x+1,y)))
+        
     if (x - 1, y) not in visted and (x-1, y) in risk:
         
         risk[(x-1,y)] = min(cost + cave[y][x-1], risk[(x-1, y)])
         temp.append((risk[(x-1,y)], (x-1,y)))
+        
     if (x, y+1) not in visted and (x, y+1) in risk:
         
         risk[(x, y+1)] = min(cost + cave[y + 1][x], risk[(x, y+1)])
         temp.append((risk[(x, y+1)], (x, y+1)))
+        
     if (x , y-1) not in visted and (x, y-1) in risk:
         
         risk[(x, y-1)] = min(cost + cave[y - 1][x], risk[(x, y-1)])
         temp.append((risk[(x,y-1)], (x, y-1)))
-    dijk(min(temp)[1][0], min(temp)[1][1], cost + min(temp)[0])
+        
+    dik(min(temp)[1][0], min(temp)[1][1], cost + min(temp)[0])
     
-dijk(0,0, 0)
+dik(0,0, 0)
 
 print(risk[(len(cave[0]) - 1, len(cave) - 1)])
