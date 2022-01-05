f11836cd26119c0939af1525c229e802bcdff976
Anson Chang <990722ansonchang@gmail.com>
2022-01-05 14:22:48 -0500

    dumbdumb

--- day20.py
+++ day20.py
@@ -12,5 +12,21 @@ for i in open(file):
         if i[-1] == '\n':
             i = i[:-1]
         image.append([i])
+
+
+directions = [[-1, 1], [0, 1], [1, 1], [-1, 0], [0,0], [1, 0],   [ -1, -1],[0, -1] , [1, - 1] ]        
+def process():
+    newimage = []
+    for i, a in enumerate(image):
+        for j, b in enumerate(a):
+            interest = [image[j + [k[0]]][i + k[1]] if j + k[0] in range(0, len(image[0])) and i + k[1] in range(0, len(image)) else '.' for k in directions)]
+            interest = [1 if k == '#' else 0 for k in image]
+            newimage.append(algo[interest])
+    return newimage
+
+for _ in range(2):
+    image = process()
     
-print(algo, image)
+return 
+        
+
